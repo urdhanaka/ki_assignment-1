fb5e09ac3f5c69de74299d6b5b6fed44034a2908
urdhanaka <urdhanaka.aptanagi@gmail.com>
2023-09-27 17:38:15 +0700

    fix: fix User entity

--- entity/user.go
+++ entity/user.go
@@ -8,13 +8,13 @@ import (
 )
 
 type User struct {
-	ID            uint64 `json:"id" gorm:"primaryKey"`
-	Name          string `json:"name" binding:"required"`
-	Email         string `json:"email" binding:"required"`
-	Password      string `json:"password" binding:"required"`
-	Profile_image string `json:"profile_image"`
-	Role          string `json:"role"`
-	Wallet        uint64 `json:"wallet"`
+	ID           uint64 `json:"id" gorm:"primaryKey"`
+	Username_AES string `json:"username_aes" binding:"required"`
+	Username_RC4 string `json:"username_rc4" binding:"required"`
+	Username_DEC string `json:"username_dec" binding:"required"`
+	Password_AES string `json:"password_aes" binding:"required"`
+	Password_RC4 string `json:"password_rc4" binding:"required"`
+	Password_DEC string `json:"password_dec" binding:"required"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) error {
