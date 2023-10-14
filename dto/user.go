5dfe26716358e3806e17cbd6d0311bb65f12f890
urdhanaka <urdhanaka.aptanagi@gmail.com>
2023-10-14 17:27:13 +0700

    fix: gofmt format

--- dto/user.go
+++ dto/user.go
@@ -3,13 +3,13 @@ package dto
 import "github.com/google/uuid"
 
 type UserCreateDto struct {
-	ID 		   uuid.UUID `gorm:"primary_key" json:"id"`
+	ID       uuid.UUID `gorm:"primary_key" json:"id"`
 	Username string    `json:"username" binding:"required"`
 	Password string    `json:"password" binding:"required"`
 }
 
 type UserUpdateDto struct {
 	ID       uuid.UUID `json:"id" binding:"required"`
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Username string    `json:"username" binding:"required"`
+	Password string    `json:"password" binding:"required"`
 }
