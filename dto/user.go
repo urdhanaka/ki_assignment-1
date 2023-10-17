4e7cd6391952cebc27d6b79174fbdd1715c91a3c
urdhanaka <urdhanaka.aptanagi@gmail.com>
2023-10-17 18:42:59 +0700

    feat: add id_card, name, number, video, CV to user entity

--- dto/user.go
+++ dto/user.go
@@ -1,11 +1,20 @@
 package dto
 
-import "github.com/google/uuid"
+import (
+	"mime/multipart"
+
+	"github.com/google/uuid"
+)
 
 type UserCreateDto struct {
-	ID       uuid.UUID `gorm:"primary_key" json:"id"`
-	Username string    `json:"username" binding:"required"`
-	Password string    `json:"password" binding:"required"`
+	ID       uuid.UUID             `gorm:"primary_key" json:"id"`
+	Name     string                `json:"name" binding:"required"`
+	Number   string                `json:"number" binding:"required"`
+	CV       *multipart.FileHeader `json:"cv" binding:"required"`
+	Video    *multipart.FileHeader `json:"video" binding:"required"`
+	ID_Card  *multipart.FileHeader `json:"id_card" binding:"required"`
+	Username string                `json:"username" binding:"required"`
+	Password string                `json:"password" binding:"required"`
 }
 
 type UserUpdateDto struct {
--- entity/user.go
+++ entity/user.go
@@ -7,14 +7,39 @@ import (
 	"gorm.io/gorm"
 )
 
+type (
+	Identity struct {
+		Name_AES    string `json:"name_aes" binding:"required"`
+		Name_RC4    string `json:"name_rc4" binding:"required"`
+		Name_DEC    string `json:"name_dec" binding:"required"`
+		Number_AES  string `json:"number_aes" binding:"required"`
+		Number_RC4  string `json:"number_rc4" binding:"required"`
+		Number_DEC  string `json:"number_dec" binding:"required"`
+		CV_AES      string `json:"cv_aes" binding:"required"`
+		CV_RC4      string `json:"cv_rc4" binding:"required"`
+		CV_DEC      string `json:"cv_dec" binding:"required"`
+		Video_AES   string `json:"video_aes" binding:"required"`
+		Video_RC4   string `json:"video_rc4" binding:"required"`
+		Video_DEC   string `json:"video_dec" binding:"required"`
+		ID_Card_AES string `json:"id_card_aes" binding:"required"`
+		ID_Card_RC4 string `json:"id_card_rc4" binding:"required"`
+		ID_Card_DEC string `json:"id_card_dec" binding:"required"`
+	}
+
+	Credential struct {
+		Username_AES string `json:"username_aes" binding:"required"`
+		Username_RC4 string `json:"username_rc4" binding:"required"`
+		Username_DEC string `json:"username_dec" binding:"required"`
+		Password_AES string `json:"password_aes" binding:"required"`
+		Password_RC4 string `json:"password_rc4" binding:"required"`
+		Password_DEC string `json:"password_dec" binding:"required"`
+	}
+)
+
 type User struct {
-	ID           uuid.UUID `gorm:"primary_key;not_null;type:char(36)" json:"id"`
-	Username_AES string    `json:"username_aes" binding:"required"`
-	Username_RC4 string    `json:"username_rc4" binding:"required"`
-	Username_DEC string    `json:"username_dec" binding:"required"`
-	Password_AES string    `json:"password_aes" binding:"required"`
-	Password_RC4 string    `json:"password_rc4" binding:"required"`
-	Password_DEC string    `json:"password_dec" binding:"required"`
+	ID uuid.UUID `gorm:"primary_key;not_null;type:char(36)" json:"id"`
+	Identity
+	Credential
 
 	// user has many files
 	Files []Files `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" binding:"required" json:"files"`
